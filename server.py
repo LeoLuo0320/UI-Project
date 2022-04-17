cb6a4a4b5a799b27eff0246b4c02becb06e28210
agh2172 <agh2172@columnbia.edu>
2022-04-17 19:58:53 -0400

    Fixed quiz page navigation

--- server.py
+++ server.py
@@ -75,23 +75,27 @@ quiz_questions = {
     "1":{
         "quiz_id" : "1",
         "question" : "Watch the video and choose what violation/foul the player made.",
+        "num": 0,
         "op1" : '24-second violation',
         "op2" : '8-second violation',
         "op3" : 'Reach-In foul',
         "op4" : 'No violation/foul',
         "ans" : '8-second violation',
         "vid" : "https://www.youtube.com/embed/QgmH11CoM80",
+        "score": 0,
         "next_lesson" : "2"
     },
     "2":{
         "quiz_id" : "2",
         "question" : "Watch the video and choose what violation/foul the player made.",
+        "num": 1,
         "op1" : '24-second violation',
         "op2" : 'Blocking foul',
         "op3" : '5-second violation',
         "op4" : 'No violation/foul',
         "ans" : '5-second violation',
         "vid" : "https://www.youtube.com/embed/exuw3rPw6vg",
+        "score" : 0,
         "next_lesson" : "end"
     }
 }
